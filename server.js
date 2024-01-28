e0105192ce9c920343ff952008000592298182e6
Piotr Węgłowski <weglowskipiotr@gmail.com>
2024-01-28 21:28:34 +0100

    Add form

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, callback) {
+      callback(null, './public');
+    },
+    filename: function (req, file, callback) {
+      callback(null, file.originalname);
+    },
+  }),
+});
 
 const app = express();
 
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
+
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/contact/send-message', upload.single('file'), (req, res) => {
+  const { author, sender, title, message } = req.body;
+
+  if (author && sender && title && message && req.file) {
+    res.render('contact', { isSent: true, file: req.file.originalname });
+  } else {
+    res.render('contact', { isError: true });
+  }
+});
 
 app.use(express.static(path.join(__dirname, '/public')));
 
