ee04e5d6cd8240c5d90d3bdf77de55d9af8cb405
Piotr Węgłowski <weglowskipiotr@gmail.com>
2024-01-28 16:12:57 +0100

    Add handlebars

--- server.js
+++ server.js
@@ -1,31 +1,44 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
-
-app.use('/users', (req, res, next) => {
-  res.show('forbidden.html');
-});
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use('/users', (req, res) => {
+  res.render('forbidden');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
+});
+
 app.get(['/', '/home'], (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history', { layout: 'dark' });
 });
 
 app.use((req, res) => {
-  res.show('notFound.html');
+  res.render('notFound');
 });
 
 app.listen(8000, () => {
